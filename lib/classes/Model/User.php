a6a567816feb11536166ccb9e629b3ac66722e84
Wob Jelsma <w.jelsma@getinteractive.nl>
2022-09-04 18:01:54 +0200

    allowed user to handle insert queries

--- lib/classes/Model/User.php
+++ lib/classes/Model/User.php
@@ -10,4 +10,10 @@ class User extends Model
     public const TABLE_NAME = 'users';
     public const QUERY_TYPE = 'select';
 
+    /** Allows insert query */
+    public static function add(array $data): bool
+    {
+        return parent::add($data);
+    }
+
 }

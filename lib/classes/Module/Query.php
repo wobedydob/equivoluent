bb754a81e1451aee6e9d4063edc39f1d6214a2f4
Wob Jelsma <w.jelsma@getinteractive.nl>
2022-09-06 22:07:34 +0200

    reworked Query module

--- lib/classes/Module/Query.php
+++ lib/classes/Module/Query.php
@@ -2,22 +2,47 @@
 
 namespace Module;
 
+use Query\QueryDelete;
+use Query\QueryInsert;
+use Query\QuerySelect;
+use Query\QueryUpdate;
+
 class Query
 {
 
-    private const SELECT = 'SELECT * FROM';
+    private const SELECT = 'SELECT';
     private const INSERT = 'INSERT INTO';
+    private const UPDATE = 'UPDATE';
+    private const DELETE = 'DELETE FROM';
 
-    /** Validates query type */
-    public function validateQueryType(string $query)
+    /** Returns Query\QuerySelect and handles MySQL SELECT statement */
+    public static function select(string $table, string $columns = '*'): QuerySelect
     {
+        // TODO: table & column validation
+        return new QuerySelect(self::SELECT . ' ' . $columns . ' FROM ' . $table);
+    }
 
-        return match ($query) {
-            'select' => self::SELECT,
-            'insert' => self::INSERT,
-            default => throw new \Exception('Invalid query type given:' . $query),
-        };
+    /** Returns Query\QueryInsert and handles MySQL INSERT statement */
+    public static function insert(string $table): QueryInsert
+    {
+        // TODO: table & column validation
+        // TODO: row & value validation
+        return new QueryInsert(self::INSERT . ' ' . $table);
+    }
 
+    /** Returns Query\QueryUpdate and handles MySQL UPDATE statement */
+    public static function update(string $table): QueryUpdate
+    {
+        // TODO: table & column validation
+        return new QueryUpdate(self::UPDATE . ' ' . $table);
+    }
+
+    // TODO: implement sql DELETE functionality
+    /** Returns Query\QueryDelete and handles MySQL DELETE statement */
+    public static function delete(string $table): QueryDelete
+    {
+        // TODO: record validation
+        return new QueryDelete(self::DELETE . ' ' . $table);
     }
 
 }

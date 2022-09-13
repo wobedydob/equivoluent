d703f49058d3d247e11aca2d414b6fcc707eab2b
Wob Jelsma <w.jelsma@getinteractive.nl>
2022-09-13 08:49:12 +0200

    removed temp functions

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -2,13 +2,3 @@
 
 include 'functions.php';
 include 'validation.php';
-
-function getUserMapper(): \Mapper\UserMapper
-{
-    return new \Mapper\UserMapper();
-}
-
-function getUserCollectionMapper(): \Mapper\CollectionMapper
-{
-    return new \Mapper\CollectionMapper(new \Mapper\UserMapper());
-}
